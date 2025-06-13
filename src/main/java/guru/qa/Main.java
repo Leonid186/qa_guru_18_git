d57f5543b9a105c8dd7b7bae19a52939b9dc662e
Леонид Зорин <l.zorin2011@yandex.ru>
2025-06-13 21:19:34 +0500

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,8 +1,8 @@
 package guru.qa;
 
 public class Main {
+    private static String msg = "Hello qa.guru!";
     public static void main(String[] args) {
-   private static String msg = "Hello qa.guru!"
         System.out.printf(msg);
 
     }
