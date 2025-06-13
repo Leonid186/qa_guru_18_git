18fa9c079fe3a5fa6632d8b0d48393ee6471dd28
Leonid <l.zorin2011@yandex.ru>
2025-06-13 21:22:10 +0500

    added getMessage () method
    Signed-off-by: Leonid <l.zorin2011@yandex.ru>

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,11 +4,12 @@ package guru.qa;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello qa.guru!");
+        System.out.printf("Hello qa.guru!!");
     }
+
     private static String getMessege() {
-        return "Hello qa.guru";
+        return "Hello qa.guru!!";
     }
-    }
+}
